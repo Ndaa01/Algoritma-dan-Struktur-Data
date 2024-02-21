1e1e3c1771cfa47d4755c547c712d18f60fe140f
Ndaa01 <ananda.idn12345@gmail.com>
2024-02-21 13:02:12 +0700

    Jobsheet1

---
+++ Pertemuan 2/Buku05.java
@@ -0,0 +1,25 @@
+public class Buku05 {
+
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi () {
+        System.out.println("Judul : " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah Halaman: " + halaman);
+        System.out.println("Sisa Stok: " + stok);
+        System.out.println("Harga: " + harga);
+    }
+
+    void terjual(int jml) {
+        stok -= jml;
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
